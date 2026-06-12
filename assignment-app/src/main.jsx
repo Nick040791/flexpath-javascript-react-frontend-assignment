89cfae315b709d81fe42adf7180afbbb0d7b827e
Nicholas Beighley <Nick040791@gmail.com>
2026-06-12 14:37:44 -0500

    updated main and stats

--- assignment-app/src/main.jsx
+++ assignment-app/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { SearchProvider } from './context/SearchContext';
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <SearchProvider>
+        <App />
+      </SearchProvider>
      </BrowserRouter> 
   </StrictMode>
 );
---
+++ assignment-app/src/utils/stats.js
@@ -0,0 +1,27 @@
+export function average(numbers){
+    if (!numbers.length) {
+        return 0;
+    } 
+    const total = numbers.reduce((sum, number) => sum + number, 0)
+    return total / numbers.length;
+};
+
+export function median(numbers){
+    if (!number.length) {return 0;}
+    
+    const sortedNumbers = [...numbers].sort((a, b) => a - b);
+    const middleIndex = Math.floor(sortedNumbers.length / 2);
+
+    if (sortedNumbers.length % 2 === 1){
+        return sortedNumbers[middleIndex];
+    }
+    return (sortedNumbers[middleIndex] - 1 + sortedNumbers[middleIndex]) / 2; 
+};
+
+export function formatNumber(value){
+    return Number(value).toLocaleString('en-us', {
+        maximumFractionDigits: 2});
+};
+
+
+
