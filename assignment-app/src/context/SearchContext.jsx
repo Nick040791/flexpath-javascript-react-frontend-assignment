09c673899ff854f9e9fcd68ebc4781ec35660872
Nicholas Beighley <Nick040791@gmail.com>
2026-06-12 14:18:35 -0500

    created context components
    -created search context component
    -created constants component
    -created stats component

---
+++ assignment-app/src/context/SearchContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useCallback, useEffect, useMemo, useState, } from "react";
+import { VALID_FILTER_TYPES } from "../utils/constants";
+
+const SearchCOntext = createContext(null);
+
+function getSafeFilterType(value){
+    if (VALID_FILTER_TYPES.has(value)) {
+        return value;
+    }
+    return '';
+};
+
+function getInitialResults(){
+    const savedResults = localStorage.getItem('searchResults');
+    if (!savedResults){return [];}
+};
+
---
+++ assignment-app/src/utils/constants.js
@@ -0,0 +1,53 @@
+export const FILTER_TYPE_OPTIONS = 
+[
+    {
+        value:'', 
+        label: 'All', 
+    },
+    {
+        value: 'gender',
+        label: 'Gender',
+    },
+        {
+        value:'operatingSystem',
+        label: 'Operating System', 
+    },
+    {
+        value: 'model',
+        label: 'Model',
+    },
+        {
+        value: 'behaviorclass',
+        label: 'Behavior Class',
+    },
+];
+
+export const VALID_FILTER_TYPES = new Set ([
+    '', 'gender', 'operatingSystem', 'model', 'behaviorclass',
+]);
+
+export const METRIC_FIELDS = [
+    {
+        key: 'App Usage Time (min/day)',
+        title: 'App Usage Time',
+        unit: 'Minutes',
+    },
+    {
+       key: 'Screen on Time',
+       title: 'Screen On Time',
+       unit: 'Hours',
+    },
+    {
+        key: 'Number of Apps Installed',
+        title: 'Number of Apps',
+        unit: 'Apps',
+    },
+    {
+        key: 'Age',
+        title: 'Age',
+        unit: 'Years',
+    },
+];
+
+
+
