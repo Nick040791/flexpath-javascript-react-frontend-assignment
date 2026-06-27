83d9d5c340139c7b03a02117336053a8db611c82
Nick040791 <nick040791@gmail.com>
2026-06-27 17:40:30 -0500

    Finish search results table

--- assignment-app/src/components/NotFound.jsx
+++ assignment-app/src/components/NotFound.jsx
@@ -1,9 +1,9 @@
 const NotFound = () => {
     return (
-        <>
-        <h2>404</h2>
+        <div className="container ">
+        <h2 className="text-danger ">404</h2>
         <h3>not found...</h3>
-        </>
+        </div>
     );
 };
 
--- assignment-app/src/pages/HomePage.jsx
+++ assignment-app/src/pages/HomePage.jsx
@@ -1,25 +1,11 @@
-const HomePage = () => { return (
-    <>
-        <h1>User Behavior Dataset</h1>
-        <p>This dataset provides a comprehensive analysis of mobile device usage patterns and user behavior classification. It contains 700 samples of user data, including metrics such as app usage time, screen-on time, battery drain, and data consumption. Each entry is categorized into one of five user behavior classes, ranging from light to extreme usage, allowing for insightful analysis and modeling.</p>
-        
-        <p>Key Features: </p>
+import Home from '../components/Home';
 
-        <ul>
-            <li>User ID: Unique identifier for each user.</li>
-            <li>Device Model: Model of the user<div> &apos; </div>s smartphone.</li>
-            <li>Operating System: The OS of the device (iOS or Android).</li>
-            <li>App Usage Time: Daily time spent on mobile applications, measured in minutes.</li>
-            <li>Screen On Time: Average hours per day the screen is active.</li>
-            <li>Battery Drain: Daily battery consumption in mAh.</li>
-            <li>Number of Apps Installed: Total apps available on the device.</li>
-            <li>Data Usage: Daily mobile data consumption in megabytes.</li>
-            <li>Age: Age of the user.</li>
-            <li>Gender: Gender of the user (Male or Female).</li>
-            <li>User Behavior Class: Classification of user behavior based on usage patterns (1 to 5).</li>
-        </ul>
-    </>
-); 
+const HomePage = () => {
+return(
+    <div className="container py-4">
+        <Home />
+    </div>
+);
 };
 
 export default HomePage;
