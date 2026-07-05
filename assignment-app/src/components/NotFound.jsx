a9efa9872ca5d854e3e180eea13e5cf1fe2ec806
Nick040791 <nick040791@gmail.com>
2026-07-05 16:34:15 -0500

    Spice up 404 and hompage with bootstrap

--- assignment-app/src/components/NotFound.jsx
+++ assignment-app/src/components/NotFound.jsx
@@ -1,11 +1,24 @@
+import { Link } from "react-router-dom";
+
+
 // The not found component for when stuff is...
 
 const NotFound = () => {
     return (
-        <div className="container py-3">
-            <h2 className="text-danger ">404</h2>
-            <h3 className="text-danger">not found...</h3>
-        </div>
+        <section className="container py-5 text-center border border-4 bg-light shadow-sm">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <h1 className="display-1 fw-bold text-danger mb-4">404</h1>
+                    <h2 className="h-4 mb-3 text-primary">Not Found</h2>
+                    <p className="text-muted mb-5">This page either does not exist or I broke it somehow :/</p>
+                    <div className="d-grip gap-2 col-md-4 mx-auto">
+                        <p className="text-muted">You don&apos;t have to go</p>
+                        <Link to="/">HOME</Link>
+                        <p className="text-muted">But you can&apos;t stay here</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
